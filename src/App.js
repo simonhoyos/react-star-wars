ea0f9f4b363e6c88ba55dd33c29b3d36b8481a46
Simon Hoyos <shmesa23@gmail.com>
2019-10-18 20:05:46 -0500

    Create global styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const theme = {
+  darkRed: '#7F0308',
+  red: '#BF3035',
+  lightRed: '#E6ACAE',
+  darkGreen: '#00330D',
+  green: '#198033',
+  lightGreen: '#99CCA6',
+  black: '#1A1A1A',
+  darkGrey: '#404040',
+  grey: '#808080',
+  lightGrey: '#C2C2C2',
+  white: '#FCFCFC',
+  shadow: '0px 15px 30px #C2C2C2',
+  shadowHover: '0px 30px 60px #C2C2C2',
+  heading1: '80px',
+  heading2: '60px',
+  heading3: '30px',
+  heading4: '24px',
+  large: '1.5em',
+  medium: '1.25em',
+  regular: '1em',
+  small: '0.75em',
+  bold: 'bold',
+  normal: 'normal',
+  semiBold: 600,
+};
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Lato', Helvetica, sans-serif;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.red};
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
   );
 }
 
